f54982c9713d94b54f943207b8f41f3478a7afdc
probuse <nprobuse@gmail.com>
2021-06-30 12:47:06 +0300

    remove duplicate room names, rooms have a capacity of 4 persons, room has unique members based on their names

--- lib/rooms.dart
+++ lib/rooms.dart
@@ -1,20 +1,18 @@
-import 'dart:convert';
 import 'dart:core';
 import 'dart:io';
 
 import 'package:args/args.dart';
-import 'package:collection/collection.dart';
 
-List<Room> rooms = []; // A room can only have 5 students and one staff member
+List<Room> rooms = []; // A room can only have 4 occupants
 
 void getUserInformation(List<String> arguments) {
   var roomNames = _getRoomNamesFromArguments(arguments);
-  var newRooms = _createRoomsFromRoomNames(roomNames);
+  var newRooms = _createRoomsFromRoomNames(roomNames.toSet().toList());
   _updateRooms(newRooms);
 
   if (rooms.isNotEmpty) {
     while (true) {
-      _addPersonToRoom();
+      _runApplication();
     }
   } else {
     print('You did not add any room names\n');
@@ -52,117 +50,168 @@ void _updateRooms(List<Room> newRooms) {
   print('${rooms.length} rooms added successfully, add people to your rooms\n');
 }
 
-void _addPersonToRoom() {
-  var userResponse = getUserResponse(
-      'Enter "student" to add student, "staff" to add a staff member');
+void _runApplication() {
+  var userResponse = _getUserResponse(
+      'Enter "student" to add student, "staff" to add a staff member, CTRL+C to quit');
   if (userResponse.toLowerCase() == 'student') {
-    // add student to room
+    // TODO This is your task
+    // We have a global variable called rooms
+    // rooms contains rooms created from the room names that you passed in the command line
+    // Your task is to add students(occupants) to these rooms
+    // each room can only have 4 occupants of type Person(this functionality is already implemented
+    // you just have to use it in your code)
+    // an occupant should not be added to a room more than once.
+    // Requirement: when creating the Student class use Person class as Interface
+    // Hint one: look at how Staff class implements the interface of Person
+    // Hint two: Borrow ideas from the functionality of adding staff. ie run the code and choose staff.
+    // Can you reproduce that functionality for the student feature? 
+    // Feel free to reuse any functions that are already created. Just don't modify them.
+    // You can add extra features if you have the above functionality working properly
+    // Happy coding
+    print('');
+    print(
+        'This feature is currently not implemented, it is your task to implement it\n');
   } else if (userResponse.toLowerCase() == 'staff') {
     // add staff to room
     _addStaffToRoom();
   } else {
-    print('Not a valid option, please try again\n');
-    _addPersonToRoom();
+    print('');
+    print(
+        '$userResponse is not a valid option, please try again\nValid options are student or staff\n');
+    _runApplication();
   }
 }
 
 void _addStaffToRoom() {
   print('\nLets add a staff member.');
-  var userResponse = getUserResponse('Enter name(s) of staff member to add');
-  var staff = Staff(names: userResponse);
-  print('');
+  var userResponse = _getUserResponse('Enter name(s) of staff member to add');
+  if (userResponse.isNotEmpty) {
+    var staff = Staff(names: userResponse);
 
-  // add staff to room
-  _selectRoom(staff);
+    print('');
+    _selectRoom(staff);
+    _addAnotherStaffMember();
+  } else {
+    print('Staff member name can not be empty\n');
+    _addAnotherStaffMember();
+  }
 }
 
-void _selectRoom(Person staff, {bool retry = false}) {
+void _selectRoom(Person person, {bool retry = false}) {
   var message = retry
       ? 'Wrong choice, please choose from the names below:'
       : 'The following are the rooms names in our records:';
   print(message);
-  print('$allRooms');
+  print('$allRooms\n');
 
-  var response = getUserResponse('Select a room to add ${staff.names}');
+  var response = _getUserResponse('Select a room to add ${person.names}');
+
   // hashCode and operator come in handle here
-  var room = Room(response.trim());
-  if (rooms.contains(room)) {
+  var selectedRoom = Room(response.trim());
+  if (rooms.contains(selectedRoom)) {
     print('');
-    room.addPerson(staff);
-    print("");
-    print("room $room has ${room.occupants.length}");
+    var currentRoomIndex = rooms.indexOf(selectedRoom);
+    var currentRoom = rooms[currentRoomIndex];
+
+    if (currentRoom.occupants.contains(person)) {
+      print(
+          'Person ${person.names} already exists in room ${currentRoom.name}\n');
+    } else {
+      var result = currentRoom.addPerson(person);
+      print(
+          'Room $currentRoom has ${currentRoom.occupants.length} occupant(s)\n');
+      if (!result) {
+        print(
+            'Room ${currentRoom.name} is already full, we can not add any more people\n');
+        _selectAFreeRoom(person);
+      }
+    }
   } else {
     print('Room $response not found, available rooms are $allRooms\n');
-    _selectRoom(staff, retry: true);
+    _selectRoom(person, retry: true);
   }
 }
 
-String get allRooms => rooms.join(', ');
+void _selectAFreeRoom(Person person) {
+  var freeRooms = rooms
+      .map((room) => room)
+      .where((room) => room.occupants.length < 4)
+      .toList();
+  print(
+      'The following are the free rooms left, select one to add ${person.names}');
+  print('${_getRoomsFromListOfRooms(freeRooms)}\n');
 
-void quitApplication(
-    {String title =
-        "We have reached the end of our program, enter 'q' or press CTRL+C to quit"}) {
-  var quit = getUserResponse(title);
-  if (quit.toLowerCase() == 'q') {
-    printResults();
-    exit(0);
-  } else {
-    print('This is not one of the provided options');
+  var response = _getUserResponse('Select a room to add ${person.names}');
+  var selectedRoom = Room(response.trim());
+
+  if (freeRooms.contains(selectedRoom)) {
     print('');
+    var currentRoomIndex = freeRooms.indexOf(selectedRoom);
+    var currentRoom = freeRooms[currentRoomIndex];
+
+    if (currentRoom.occupants.contains(person)) {
+      print(
+          'Person ${person.names} already exists in room ${currentRoom.name}\n');
+    } else {
+      currentRoom.addPerson(person);
+      print(
+          'Room $currentRoom has ${currentRoom.occupants.length} occupant(s)\n');
+    }
+  } else {
+    print(
+        'Room $response not found, available rooms are ${_getRoomsFromListOfRooms(freeRooms)}\n');
   }
 }
 
-String getUserResponse(String question) {
+void _addAnotherStaffMember() {
+  var userResponse =
+      _getUserResponse('Do you want to add another staff member? yes/no');
+  if (userResponse.toLowerCase() == 'yes') {
+    _addStaffToRoom();
+  } else if (userResponse.toLowerCase() == 'no') {
+    print('Lets continue then ...\n');
+  } else {
+    print(
+        '$userResponse is not a valid option.\nValid options are yes or no\n');
+  }
+}
+
+/* Helper functions */
+String _getUserResponse(String question) {
   stdout.write('$question: \n');
   return stdin.readLineSync();
 }
 
-/// returns a list of foods from a string of foods separated by space
-List<String> getFavoriteFoods(String favoriteFoodString) {
-  return favoriteFoodString.split(' ');
+String get allRooms => _getRoomsFromListOfRooms(rooms);
+String _getRoomsFromListOfRooms(List<Room> rooms) {
+  return rooms.join(', ');
 }
 
-var retries = 3;
-int getUserAge() {
-  stdout.write('Enter your age: \n');
-  var response = stdin.readLineSync();
-  int age;
-  try {
-    age = int.parse(response);
-    return age;
-  } catch (e) {
-    if (retries > 0) {
-      retries -= 1;
-      print('Invalid age, you have $retries retries left');
-      return getUserAge();
-    } else {
-      print(
-          'Invalid, you have used all your retries, your age wont be captured');
-      return null;
-    }
-  }
-}
-
-void printResults() {
-  print('\n');
-  print('');
-}
-
+/* Classes */
 abstract class Person {
   String names;
 }
 
-abstract class Building {
-  String name;
-  List<Room> rooms;
+class Staff implements Person {
+  @override
+  String names;
 
-  void addRoom(Room room);
-  void removeRoom(Room room);
+  Staff({this.names});
+
+  // used to compare if two objects are the same
+  @override
+  bool operator ==(Object other) {
+    return other is Staff && other.names == names;
+  }
+
+  // gives hashcode for this object(used when comparing objects)
+  @override
+  int get hashCode => names.hashCode;
 }
 
 class Room {
   String name;
-  List<Person> occupants = [];
+  Set<Person> occupants = {};
 
   Room(this.name);
 
@@ -177,40 +226,12 @@ class Room {
   @override
   int get hashCode => name.hashCode;
 
-  void addPerson(Person person) {
-    occupants.add(person);
+  bool addPerson(Person person) {
+    if (occupants.length < 4) {
+      occupants.add(person);
+      return true;
+    } else {
+      return false;
+    }
   }
-
-  void removePerson(Person person) {}
-}
-
-class Staff implements Person {
-  @override
-  String names;
-
-  Staff({this.names});
-}
-
-class Hostel implements Building {
-  @override
-  String name;
-
-  @override
-  List<Room> rooms;
-
-  @override
-  void addRoom(Room room) {
-    // TODO: implement addRoom
-  }
-
-  @override
-  void removeRoom(Room room) {
-    // TODO: implement removeRoom
-  }
-}
-
-void _createHostel(String name, List<Room> rooms) {
-  var hostel = Hostel();
-  hostel.name = name;
-  hostel.rooms = rooms;
 }
